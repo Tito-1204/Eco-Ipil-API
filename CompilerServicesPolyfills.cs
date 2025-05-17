a4831ace09dfa76d07fb902ef1fe195ecfe85681
Tito-1204 <t1t0124capewando@gmail.com>
2025-05-17 16:32:40 +0100

    Atualizando API EcoIpil antes do deploy

---
+++ CompilerServicesPolyfills.cs
@@ -0,0 +1,27 @@
+// File: CompilerServicesPolyfills.cs
+#if !NET7_0_OR_GREATER
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    internal sealed class RequiredMemberAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    internal sealed class CompilerFeatureRequiredAttribute : Attribute
+    {
+        public CompilerFeatureRequiredAttribute(string featureName)
+        {
+            FeatureName = featureName;
+        }
+        public string FeatureName { get; }
+        public bool IsOptional { get; init; }
+    }
+}
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    internal sealed class ScopedRefAttribute : Attribute { }
+}
+
+#endif
